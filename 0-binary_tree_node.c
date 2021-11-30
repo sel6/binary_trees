f3a7e5643c17bb85f3e8cd6b0773a280d0ffdf3e
Selam <84750917+sel6@users.noreply.github.com>
2021-11-30 14:25:03 +0300

    upd

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
  
     binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
  if (newnode == NULL)
-  return null;
+  return NULL;
     newnode->n = value;
     newnode->parent = parent;
     newnode->left = NULL;
